284f0a64cfa085476c3a19702191a495d04240ac
Ignacio Bisio <ibisio@gocloud.la>
2026-03-17 09:19:20 -0300

    fix(readme): update examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -13,12 +13,12 @@ module "wrapper_kinesis_stream" {
 
       # Encryption
       # encryption_type = "KMS" # Default KMS
-      # kms_key_id = "alias/aws/kinesis" # Default AWS KMS for Kinesis
+      # kms_key_id      = "alias/aws/kinesis" # Default AWS KMS for Kinesis
 
       # Policy to create cross-account access
-      # create_policy = true # Default false
+      # create_policy                  = true # Default false
       # aws_account_policy_destination = "XXXXX" # Number of the account id
-      # policy_actions = ["kinesis:DescribeStreamSummary", "kinesis:DescribeStreamSummary"] # Custom Actions
+      # policy_actions                 = ["kinesis:DescribeStreamSummary", "kinesis:DescribeStreamSummary"] # Custom Actions
     }
     "on-demand" = {
       retention_period = 720 # 30 dias
